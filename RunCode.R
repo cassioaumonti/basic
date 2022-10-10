b776b25edf823128452008f77ae68c054d4e4cad
cassioaumonti <cassioaumonti@yahoo.com>
2022-10-10 17:12:33 -0400

    test

---
+++ RunCode.R
@@ -0,0 +1,20 @@
+
+rmarkdown::render(input = "Monti_Patnaik_project2T_ST558.Rmd",
+                  output_file = "README.md",
+                  output_format = "github_document",
+                  output_options = list(
+                    name_value_pairs = "value",
+                    or_something = TRUE,
+                    toc = TRUE,
+                    df_print = "kable"
+                  ),
+                  runtime = "static",
+                  clean = TRUE,
+                  params = NULL,
+                  knit_meta = NULL,
+                  envir = parent.frame(),
+                  run_pandoc = TRUE,
+                  quiet = FALSE,
+                  encoding = "UTF-8"
+)
+
